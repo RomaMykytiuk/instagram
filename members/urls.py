721a80b4bfa907a6c463444a29feaef29390a529
Микитюк Роман <romamykytiuk0604@gmail.com>
2024-12-18 14:29:38 +0200

    beta_1.1

--- members/urls.py
+++ members/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
    path('members/', views.members, name='members'),
-  #path('login/', views.login_view, name='login'),
+   path('login/', views.login_view, name='login'),
    path("accounts/emailsingup/" ,views.register, name="register"),
   # path('profile/', profile_view, name='profile'),
 
--- members/views.py
+++ members/views.py
@@ -1,14 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib import messages
-from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
-
-
-
-
 from .models import User
 
 def members(request):
@@ -17,33 +12,27 @@ def members(request):
 
 def register(request):
     if request.method == "POST":
-        form=UserRegistrationForm(request.POST,request.FILES)
+        form = UserRegistrationForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, 'Account created!')
             return redirect('login')
         else:
-            messages.error(request,"Помилка!")
+            messages.error(request, "Помилка!")
     else:
-        form=UserRegistrationForm()
-    return render(request, "register.html", {"form":form})
+        form = UserRegistrationForm()
+    return render(request, "register.html", {"form": form})
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')  # Додаємо цю змінну
 
-
-
-
-# def login_view(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         if username == 'test' and password == 'password123':
-#             return HttpResponse("Ви успішно увійшли!")
-#         else:
-#             return HttpResponse("Невірний логін або пароль!")
-#     return render(request, 'login.html')
-
-
-
+        if username == 'test' and password == 'password123':
+            return HttpResponse("Ви успішно увійшли!")
+        else:
+            return HttpResponse("Невірний логін або пароль!")
+    return render(request, 'login.html')
 
 # def profile_view(request):
 #     profile, created = Profile.objects.get_or_create(id=1)
